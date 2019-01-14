fd39d52e54640f30ebb6c82f5411d03c88be41d1
Ilan Peer <ilan.peer@intel.com>
2019-01-14 15:57:44 +0200

    [BUGFIX] iwlwifi: mvm: Do not return an error value on HW restart
    When HW restart is requested but not started yet, commands would not
    be sent to the FW, and some function calls would return an error. In
    case of iwl_mvm_mac_sta_state() returning an error value when a
    station is removed can lead to an unneeded warning in
    __sta_info_destroy_part2().
    
    Handle this by setting the return value to 0, in case HW restart is
    in progress.
    
    type=bugfix
    ticket=jira:WIFI-14566
    fixes=unknown
    
    Change-Id: Ib830d4b4e726351f8494ac7988e5377c49d7f770
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/205240
    automatic-review: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Coelho, Luciano <luciano.coelho@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/207204
    x-iwlwifi-stack-dev: 251a7e4288fd8dd356a913e11ec3263b438f41d1

--- drivers/net/wireless/intel/iwlwifi/mvm/mac80211.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/mac80211.c
@@ -3267,6 +3267,10 @@ static int iwl_mvm_mac_sta_state(struct ieee80211_hw *hw,
 				ret = rm_ret;
 		}
 
+		if (unlikely(ret &&
+			     test_bit(IWL_MVM_STATUS_HW_RESTART_REQUESTED,
+				      &mvm->status)))
+			ret = 0;
 	} else {
 		ret = -EIO;
 	}
